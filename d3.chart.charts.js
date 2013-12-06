4a212d8dbe96641c9097914b2ce809d473d89ce2
Carl Sutherland <carl@humblesoftware.com>
2013-12-05 19:52:46 -0800

    Giving thanks.

--- d3.chart.charts.js
+++ d3.chart.charts.js
@@ -1,6 +1,8 @@
 /**
  * Licensed under the MIT License.
  * (c) 2013 Carl Sutherland
+ *
+ * Inspired by various d3 examples of Mike Bostock and d3.chart examples of Irene Ros and @jugglingmike.
  */
 d3.chart("Histogram", {
 
--- header.js
+++ header.js
@@ -1,4 +1,6 @@
 /**
  * Licensed under the MIT License.
  * (c) 2013 Carl Sutherland
+ *
+ * Inspired by various d3 examples of Mike Bostock and d3.chart examples of Irene Ros and @jugglingmike.
  */
